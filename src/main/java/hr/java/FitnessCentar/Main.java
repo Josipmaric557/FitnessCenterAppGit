a14d3c9001189686568a828979626b6cc4112a6e
Josipmaric557 <jmaric3@algebra.hr>
2026-05-02 13:33:01 +0200

    dfgbh

--- src/main/java/hr/java/FitnessCentar/Main.java
+++ src/main/java/hr/java/FitnessCentar/Main.java
@@ -2,14 +2,13 @@ package hr.java.FitnessCentar;
 
 import hr.java.FitnessCentar.Util.DB_Connection;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class Main {
     public static void main(String[] args) {
-        try {
-            System.out.println(DB_Connection.getInstance());
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
+
+        System.out.println("Hello");
     }
 }
